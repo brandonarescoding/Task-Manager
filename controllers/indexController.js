d5f8830046256b2464f0238ae18db59b0a5a9e08
Brandon Kamga <brandoniscoding4@gmail.com>
2024-04-25 10:03:56 +0100

    index(route, controller) configuration

---
+++ controllers/indexController.js
@@ -0,0 +1,9 @@
+// indexcontroller.js
+
+const indexController = {
+    getIndex: (req, res) => {
+        res.render('index');
+    }
+};
+
+module.exports = indexController;
--- server.js
+++ server.js
@@ -4,35 +4,37 @@
 const express = require('express');
 const bodyParser = require('body-parser'); 
 const path = require('path'); 
-const dontenv = require('dotenv'); 
+const dotenv = require('dotenv'); 
+
+dotenv.config();
 
 // Initialization of express app
 const app = express();
+
+// Static files 
+app.use(express.static(path.join(__dirname, 'public')));
 
 // EJS template engine 
 app.set('view engine', 'ejs'); 
 app.set('views', path.join(__dirname, 'views')); 
 
 // Middleware for parsing the request body
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Static files 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes import
-const indexRouter = require('./routes/index');
-const authRouter = require('./routes/auth');
-const usersRouter = require('./routes/users');
-const adminRouter = require('./routes/admin');
-const tasksRouter = require('./routes/tasks');
+const indexRouter = require('./routes/indexRouter');
+//const authRouter = require('./routes/auth');
+//const usersRouter = require('./routes/users');
+//const adminRouter = require('./routes/admin');
+//const tasksRouter = require('./routes/tasks');
 
 // Routes
 app.use('/', indexRouter);
-app.use('/auth', authRouter);
-app.use('/users', usersRouter);
-app.use('/admin', adminRouter);
-app.use('/tasks', tasksRouter);
+//app.use('/auth', authRouter);
+//app.use('/users', usersRouter);
+//app.use('/admin', adminRouter);
+//app.use('/tasks', tasksRouter);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
